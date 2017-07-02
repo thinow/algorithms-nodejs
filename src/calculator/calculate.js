3ce71cf7e4a5125b0100de5b94d8e562b53e23a1
Thierry Nowak <thinow@gmail.com>
2017-07-02 18:10:13 +0200

    calculator: handle all operations

--- src/calculator/calculate.js
+++ src/calculator/calculate.js
@@ -2,10 +2,23 @@ const radix = 10;
 
 const int = left => parseInt(left, radix);
 
+const operations = [
+  { key: '+', then: (left, right) => left + right },
+  { key: '-', then: (left, right) => left - right },
+  { key: '*', then: (left, right) => left * right },
+  { key: '/', then: (left, right) => left / right },
+];
+
+function compute(left, operator, right) {
+  return operations
+    .find(({ key }) => (key === operator))
+    .then(int(left), int(right));
+}
+
 const calculate = ([left, operator, right, ...rest]) => {
   if (!operator) return int(left);
 
-  const computed = int(left) + int(right);
+  const computed = compute(left, operator, right);
   return calculate([computed, ...rest]);
 };
 
--- src/calculator/calculate.test.js
+++ src/calculator/calculate.test.js
@@ -3,7 +3,11 @@ import calculate from './calculate';
 describe('Calculator', () => {
   it('One digit', () => expect(calculate(['1'])).toBe(1));
 
-  it('Simple addition', () => expect(calculate(['1', '+', '2'])).toBe(3));
+  it('Simple sum', () => expect(calculate(['1', '+', '2'])).toBe(3));
+  it('Simple minus', () => expect(calculate(['8', '-', '3'])).toBe(5));
+  it('Simple times', () => expect(calculate(['3', '*', '5'])).toBe(15));
+  it('Simple divide', () => expect(calculate(['8', '/', '2'])).toBe(4));
 
   it('2 additions', () => expect(calculate(['1', '+', '2', '+', '3'])).toBe(6));
+  it('Mix', () => expect(calculate(['4', '/', '2', '-', '1'])).toBe(1));
 });
