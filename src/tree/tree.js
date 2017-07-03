0ac088aa876405050f4b7821498d9e74cc8d4bd9
Thierry Nowak <thinow@gmail.com>
2017-07-03 22:44:53 +0200

    tree: sort

--- src/tree/tree.js
+++ src/tree/tree.js
@@ -11,15 +11,24 @@ const arrayOf = ({ value, left, right } = {}) => {
 export const stringify = tree => arrayOf(tree).join(',');
 
 export const insert = (value, tree = {}) => {
-  if (tree.value === undefined) return { value };
-
-  if (value < tree.value) {
-    return { ...tree, left: insert(value, tree.left) };
+  if (tree.value === undefined) {
+    tree.value = value;
+  } else if (value < tree.value) {
+    tree.left = insert(value, tree.left);
+  } else {
+    tree.right = insert(value, tree.right);
   }
-
-  return { ...tree, right: insert(value, tree.right) };
+  return tree;
 };
 
-export const sort = () => ({});
+export const sort = ({ value, left, right } = {}, accumulator = {}) => {
+  if (value === undefined) return accumulator;
+
+  insert(value, accumulator);
+  sort(left, accumulator);
+  sort(right, accumulator);
+
+  return accumulator;
+};
 
 export default { sort, insert, stringify };
--- src/tree/tree.test.js
+++ src/tree/tree.test.js
@@ -1,4 +1,4 @@
-import { insert, stringify } from './tree';
+import { stringify, insert, sort } from './tree';
 
 describe('Tree', () => {
   describe('Print', () => {
@@ -19,8 +19,8 @@ describe('Tree', () => {
     it('Complex tree', () => {
       // given
       const tree = {
+        left: { left: { value: 0 }, value: 1 },
         value: 2,
-        left: { value: 1, left: { value: 0 } },
         right: { value: 3, right: { value: 4, right: {} } },
       };
 
@@ -30,38 +30,76 @@ describe('Tree', () => {
   });
 
   describe('Insert', () => {
-    it('Unique value', () => expect(stringify(insert(5))).toEqual('5'));
+    it('Unique value', () => {
+      // when
+      const tree = insert(5);
+      // then
+      expect(stringify(tree)).toEqual('5');
+    });
 
     it('Insert left', () => {
       // given
       const tree = { value: 10 };
-
+      // when
+      const result = insert(5, tree);
       // then
-      expect(stringify(insert(5, tree))).toEqual('5,10');
+      expect(stringify(result)).toEqual('5,10');
     });
 
     it('Insert 2nd level left', () => {
       // given
       const tree = { value: 10, left: { value: 5 } };
 
+      // when
+      const result = insert(1, tree);
       // then
-      expect(stringify(insert(1, tree))).toEqual('1,5,10');
+      expect(stringify(result)).toEqual('1,5,10');
     });
 
     it('Insert right', () => {
       // given
       const tree = { value: 10 };
-
+      // when
+      const result = insert(20, tree);
       // then
-      expect(stringify(insert(20, tree))).toEqual('10,20');
+      expect(stringify(result)).toEqual('10,20');
     });
 
     it('Insert 2nd level right', () => {
       // given
       const tree = { value: 10, right: { value: 20 } };
+      // when
+      const result = insert(30, tree);
+      // then
+      expect(stringify(result)).toEqual('10,20,30');
+    });
+  });
+
+  describe('Sort', () => {
+    it('Empty', () => expect(stringify(sort())).toEqual(''));
+
+    it('Simple tree', () => {
+      // given
+      const tree = {
+        value: 3,
+        left: { value: 2 },
+        right: { value: 1 },
+      };
 
       // then
-      expect(stringify(insert(30, tree))).toEqual('10,20,30');
+      expect(stringify(sort(tree))).toEqual('1,2,3');
+    });
+
+    it('Complex tree', () => {
+      // given
+      const tree = {
+        left: { left: { value: 8 }, value: 1 },
+        value: 7,
+        right: { value: 4, right: { value: 3, right: {} } },
+      };
+
+      // then
+      expect(stringify(sort(tree))).toEqual('1,3,4,7,8');
     });
   });
 });
